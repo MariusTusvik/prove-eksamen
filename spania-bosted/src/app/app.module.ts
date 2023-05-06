3143ecaed8d4c744773215b6169b9023d3c7e155
MariusTusvik <tusvik.marius@gmail.com>
2023-05-06 13:23:04 +0200

    TailwindCSS, Angular Material and file structure

--- spania-bosted/src/app/app.module.ts
+++ spania-bosted/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './Pages/front-page/front-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
